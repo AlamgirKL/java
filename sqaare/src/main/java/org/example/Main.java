7c443ebfbcc3b56214dc4410030c522ef2110edd
AlamgirKL <alamgirkabir452@gmail.com>
2023-03-04 21:46:25 +0600

    Angle Pattern

--- sqaare/src/main/java/org/example/Main.java
+++ sqaare/src/main/java/org/example/Main.java
@@ -36,24 +36,68 @@ public class Main {
 //            System.out.println();
 //        }
 
-//        Right Angle (left)
+//              Pyramid
 
-        int rows = 5, k = 0;
+//        int rows = 5, k = 0;
+//
+//        for (int i = 1; i <= rows; ++i, k = 0) {
+//            for (int space = 1; space <= rows - i; ++space) {
+//                System.out.print("  ");
+//            }
+//
+//            while (k != 2 * i - 1) {
+//                System.out.print("* ");
+//                ++k;
+//            }
+//
+//            System.out.println();
+//        }
 
-        for (int i = 1; i <= rows; ++i, k = 0) {
-            for (int space = 1; space <= rows - i; ++space) {
+
+//        Right Angle (Right )
+//
+//        int rows = 5;
+//
+//        for (int i = rows; i >= 1; --i) {
+//            for (int j = 1; j <= i; ++j) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+// Error  or pyramid AB C D E F GH
+//        int rows = 5;
+//        int asciiValue = 65;
+//
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = rows - i; j > 1; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print((char) asciiValue + " ");
+//                asciiValue++;
+//            }
+//            System.out.println();
+//        }
+
+
+//        right Angle (Left )
+//
+        int rows = 5;
+        System.out.println("## Printing the pattern ##");
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = rows; j > i; j--)
+            {
                 System.out.print("  ");
             }
 
-            while (k != 2 * i - 1) {
+            for (int k = 1; k <= i; k++)
+            {
                 System.out.print("* ");
-                ++k;
             }
-
             System.out.println();
         }
-
-
-
     }
 }
